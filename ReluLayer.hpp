7f6d17d50d393363df80afbec6bd280bc8a688c0
lr94 <lucarobbiano@gmail.com>
2019-02-22 22:56:25 +0100

    Implement ReluLayer

---
+++ ReluLayer.hpp
@@ -0,0 +1,67 @@
+#ifndef CNN_RELULAYER_HPP
+#define CNN_RELULAYER_HPP
+
+#include <cmath>
+#include "Vol.hpp"
+#include "LayerBase.hpp"
+
+class ReluLayer : public LayerBase
+{
+public:
+    ReluLayer()
+    {
+        input = output = dInput = dOutput = nullptr;
+    }
+
+    ~ReluLayer()
+    {
+        delete output;
+        delete dOutput;
+    }
+
+    void forward() override
+    {
+        size_t inputSize = input->getDataSize();
+
+        for (unsigned i = 0; i < inputSize; i++)
+            output->set(i, relu(input->get(i)));
+    }
+
+    void backward() override
+    {
+        size_t inputSize = input->getDataSize();
+
+        for (unsigned i = 0; i < inputSize; i++)
+            dInput->set(i, dOutput->get(i) * relu_derivative(output->get(i)));
+    }
+
+protected:
+    void prepend(LayerBase *previousLayer) override
+    {
+        LayerBase::prepend(previousLayer);
+
+        delete output;
+        delete dOutput;
+
+        // The output has the same shape of the input
+        output = new Vol<>(input->depth(), input->height(), input->width());
+        dOutput = new Vol<>(input->depth(), input->height(), input->width());
+    }
+
+private:
+    static inline float relu(float x)
+    {
+        return std::fmax(0, x);
+    }
+
+    static inline float relu_derivative(float x)
+    {
+        if (x < 0.0f)
+            return 0.0f;
+        else
+            return 1.0f;
+    }
+};
+
+
+#endif //CNN_RELULAYER_HPP
