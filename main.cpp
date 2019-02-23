fa213de742e56e5198ba85ef66fe1858f72d4088
lr94 <lucarobbiano@gmail.com>
2019-02-23 12:23:18 +0100

    Test stuff

--- main.cpp
+++ main.cpp
@@ -10,36 +10,25 @@
 
 int main()
 {
-    Vol<> v(2, 1, 1);
-    v = {0.7, -9.6};
-
     InputLayer inputLayer(2, 1, 1);
     FullyConnectedLayer fcc1(4);
-    FullyConnectedLayer fcc2(8);
-    FullyConnectedLayer fcc3(3);
-    SigmoidLayer s;
+    SigmoidLayer sig1;
+    FullyConnectedLayer fcc2(1);
     RegressionLayer r;
 
-    Net net;
-    net.appendLayer(&inputLayer)
-       .appendLayer(&fcc1)
-       .appendLayer(&fcc2)
-       .appendLayer(&fcc3)
-       .appendLayer(&s)
-       .appendLayer(&r);
+    Net network;
+    network.appendLayer(&inputLayer)
+           .appendLayer(&fcc1)
+           .appendLayer(&sig1)
+           .appendLayer(&fcc2)
+           .appendLayer(&r);
 
-    Vol<> &input = net.getInput();
-    input[0][0][0] = 0.7;
-    input[1][0][0] = -9.6;
+    r.setY(std::vector<float>{0});
+    network.forward();
+    network.backward();
 
-    r.setY(std::vector<float>{0, 0, 0});
-
-    net.forward();
-    net.backward();
-
-    std::cout << net.getOutput() << std::endl;
-
-    std::cout << "Loss: " << net.getLoss() << std::endl;
+    std::cout << network.getOutput() << std::endl;
+    std::cout << "Loss: " << network.getLoss() << std::endl;
 
     return 0;
 }
