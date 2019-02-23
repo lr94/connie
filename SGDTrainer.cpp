d7c42a3b5cda9de54ec2f5209b94b33a38027525
lr94 <lucarobbiano@gmail.com>
2019-02-23 14:15:36 +0100

    Implement gradient descent

--- SGDTrainer.cpp
+++ SGDTrainer.cpp
@@ -2,10 +2,16 @@
 
 void SGDTrainer::updateLayerParams(std::vector<float> &params, std::vector<float> &gradient) const
 {
+    size_t size = params.size();
 
+    for (unsigned i = 0; i < size; i++)
+        params[i] -= learningRate * gradient[i];
 }
 
 void SGDTrainer::updateLayerParams(Vol<> &params, Vol<> &gradient) const
 {
+    size_t size = params.getDataSize();
 
+    for (unsigned i = 0; i < size; i++)
+        params.addAt(i, -learningRate * gradient.get(i));
 }
